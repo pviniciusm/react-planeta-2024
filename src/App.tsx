e5e21929e1264a302f29256287c1cbbe2d14bd18
Paulo <pvinics@gmail.com>
2024-02-08 19:07:46 -0300

    add header e card de atrações

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { Provider } from "react-redux";
-import "./App.css";
 import { persistedStore, store } from "./config/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { Header } from "./components/Header";
+import { Container } from "@mui/material";
+import { CardAtracao } from "./components/CardAtracao";
 
 function App() {
     return (
         <>
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistedStore}>
-                    <h1>Teste</h1>
+                    <Header />
+                    <Container>
+                        <h1>Teste</h1>
+                        <CardAtracao />
+                    </Container>
                 </PersistGate>
             </Provider>
         </>
